dcd23349bc47b4e29d409d5c03e4d3e395ee9c7f
William Porter <Willporterdesign@outlook.com>
2026-01-26 11:04:46 +0700

    small updates

--- frontend/src/lib/components/DMPanel.svelte
+++ frontend/src/lib/components/DMPanel.svelte
@@ -458,7 +458,7 @@
 					on:click={() => fileInput?.click()}
 					title="Attach file"
 				>
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21.5 2v6h-6M2.5 22v-6h6M2 11.5a10 10 0 0 0 19.8 4.3M22 12.5a10 10 0 0 0-19.8-4.2"></path></svg>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M13 2H7a2 2 0 0 0-2 2v9a7 7 0 0 0 7 7 7 7 0 0 0 7-7V4a1 1 0 0 0-1-1h0"></path><line x1="7" y1="13" x2="19" y2="13"></line></svg>
 				</button>
 				<textarea
 					bind:this={textareaElement}
